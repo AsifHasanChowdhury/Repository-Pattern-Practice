4bf8ed9a9e98e6be3102c9572e6a23aebbf385f0
Asif Hasan <72686397+AsifHasanChowdhury@users.noreply.github.com>
2022-11-10 17:49:55 +0600

    update

--- RepositoryPatternPractice/Models/Business Objet/Users.cs
+++ RepositoryPatternPractice/Models/Business Objet/Users.cs
@@ -1,6 +1,27 @@
-﻿namespace RepositoryPatternPractice.Models.Business_Objet
+﻿using Microsoft.Build.Framework;
+
+namespace RepositoryPatternPractice.Models.Business_Objet
 {
     public class Users
     {
+        [Required]
+        int id { get; set; }
+
+        [Required]
+        string username { get; set; }
+
+        [Required]
+        string userEmail { get; set; }
+
+        [Required]        
+        string password { get; set; }
+
+        [Required]
+        string userRole { get; set; }
+
+
+
+
+       
     }
 }
